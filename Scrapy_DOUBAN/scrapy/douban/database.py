9ebb054a278eefe11fe8f02c8dd644b131cc0665
赵梓钧 <38558632+uchihazzj@users.noreply.github.com>
2022-05-06 19:36:56 +0800

    Update database.py

--- Scrapy_DOUBAN/scrapy/douban/database.py
+++ Scrapy_DOUBAN/scrapy/douban/database.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 23:41:38 2019
-
-@author: liudiwei
-"""
-
 import pymysql
 
-MYSQL_DB = 'douban'
-MYSQL_USER = 'root'
-MYSQL_PASS = 'hhss1009..'
-MYSQL_HOST = 'localhost'
+# 以下根据本地环境自己填
+MYSQL_DB = ''
+MYSQL_USER = ''
+MYSQL_PASS = ''
+MYSQL_HOST = ''  # 'localhost'
 
 connection = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER,
                              password=MYSQL_PASS, db=MYSQL_DB,
